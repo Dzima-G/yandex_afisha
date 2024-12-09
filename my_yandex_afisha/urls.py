0663fbfbb0409d0ededd5ed327da3568060c89db
Dzima-G <dgukalin@gmail.com>
2024-12-09 19:52:44 +0300

    Add blank start page

--- my_yandex_afisha/urls.py
+++ my_yandex_afisha/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_yandex_afisha import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_page),
 ]
