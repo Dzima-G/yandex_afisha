b3569ca67b4d161b7a95e77a297afa1687d615f6
Dzima-G <dgukalin@gmail.com>
2024-12-11 19:17:58 +0300

    Create places app

--- my_yandex_afisha/urls.py
+++ my_yandex_afisha/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from my_yandex_afisha import views
+from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
