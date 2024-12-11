cb002db0acb83f653c0dfd6adfc988ace2e0d239
Dzima-G <dgukalin@gmail.com>
2024-12-11 20:25:30 +0300

    Add Place model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateField()
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
