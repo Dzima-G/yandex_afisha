cead7c7536ffb6956086b78a95999c0e74699cc0
Dzima-G <dgukalin@gmail.com>
2024-12-21 11:16:47 +0300

    Added display of image in the model

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from .models import Place, Image
-
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 admin.site.register(Image)
 
 class ImageInline(admin.TabularInline):
     model = Image
-    fields = ('image', 'geeks_field')
+    readonly_fields = ["preview"]
+    fields = ('image', 'preview', 'geeks_field')
+
+    def preview(self, obj):
+        return format_html("<a>{}</a>",
+                    mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
+                    )
 
 @admin.register(Place)
 class PlaceForm(admin.ModelAdmin):
